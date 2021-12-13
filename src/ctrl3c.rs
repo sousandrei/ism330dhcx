82e9b931f8aa4bd8adfaf030bd70cc775981092f
Andrei Sousa <sousandrei@gmail.com>
2021-12-13 19:23:45 +0100

    ✨ making address configurable

--- src/ctrl3c.rs
+++ src/ctrl3c.rs
@@ -8,23 +8,26 @@ use crate::Register;
 /// Contains memory reboot, block data update, interruct activation level, push-pull/open-drain selection on INT1 and INT2 pins
 /// SPI Serial Interface Mode selection, register address automatically incrementation and software reset
 #[derive(Debug)]
-pub struct Ctrl3C(u8);
+pub struct Ctrl3C {
+    pub address: u8,
+    value: u8,
+}
 
 impl fmt::Display for Ctrl3C {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "{}", self.value)
     }
 }
 
 impl fmt::Binary for Ctrl3C {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:b}", self.0)
+        write!(f, "{:b}", self.value)
     }
 }
 
 impl fmt::LowerHex for Ctrl3C {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::LowerHex::fmt(&self.0, f)
+        fmt::LowerHex::fmt(&self.value, f)
     }
 }
 
@@ -87,54 +90,54 @@ pub const SW_RESET: u8 = 0;
 impl Register for Ctrl3C {}
 
 impl Ctrl3C {
-    pub fn new(bits: u8) -> Self {
-        Ctrl3C(bits)
+    pub fn new(value: u8, address: u8) -> Self {
+        Ctrl3C { address, value }
     }
 
     pub fn boot(&mut self) -> bool {
-        self.0 & (1 << BOOT) != 0
+        self.value & (1 << BOOT) != 0
     }
 
     pub fn set_boot<I2C>(&mut self, i2c: &mut I2C, value: bool) -> Result<(), I2C::Error>
     where
         I2C: Write,
     {
-        self.0 &= !(1 << BOOT);
-        self.0 |= u8::from(value) << BOOT;
-        self.write(i2c, ADDR, self.0)
+        self.value &= !(1 << BOOT);
+        self.value |= u8::from(value) << BOOT;
+        self.write(i2c, self.address, ADDR, self.value)
     }
 
     pub fn bdu(&mut self) -> bool {
-        self.0 & (1 << BDU) != 0
+        self.value & (1 << BDU) != 0
     }
 
     pub fn set_bdu<I2C>(&mut self, i2c: &mut I2C, value: bool) -> Result<(), I2C::Error>
     where
         I2C: Write,
     {
-        self.0 &= !(1 << BDU);
-        self.0 |= (value as u8) << BDU;
-        self.write(i2c, ADDR, self.0)
+        self.value &= !(1 << BDU);
+        self.value |= (value as u8) << BDU;
+        self.write(i2c, self.address, ADDR, self.value)
     }
 
     pub fn sw_reset<I2C>(&mut self, i2c: &mut I2C) -> Result<(), I2C::Error>
     where
         I2C: Write,
     {
-        self.0 |= 1 << SW_RESET;
-        self.write(i2c, ADDR, self.0)
+        self.value |= 1 << SW_RESET;
+        self.write(i2c, self.address, ADDR, self.value)
     }
 
     pub fn if_inc(&mut self) -> bool {
-        self.0 & (1 << IF_INC) != 0
+        self.value & (1 << IF_INC) != 0
     }
 
     pub fn set_if_inc<I2C>(&mut self, i2c: &mut I2C, value: bool) -> Result<(), I2C::Error>
     where
         I2C: Write,
     {
-        self.0 &= !(1 << IF_INC);
-        self.0 |= (value as u8) << IF_INC;
-        self.write(i2c, ADDR, self.0)
+        self.value &= !(1 << IF_INC);
+        self.value |= (value as u8) << IF_INC;
+        self.write(i2c, self.address, ADDR, self.value)
     }
 }
