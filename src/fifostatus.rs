c4e4ee9431ae5b5e92204438437f527fbe76b1c3
Gaute Hope <eg@gaute.vetsj.com>
2021-12-13 21:08:31 +0100

    feat: adapt to custom address

--- src/fifostatus.rs
+++ src/fifostatus.rs
@@ -3,15 +3,17 @@ use embedded_hal::blocking::i2c::WriteRead;
 use crate::Register;
 
 /// The FIFO_STATUS registers.
-pub struct FifoStatus;
+pub struct FifoStatus {
+    pub address: u8,
+}
 
 pub const ADDR: u8 = 0x3a_u8;
 
 impl Register for FifoStatus {}
 
 impl FifoStatus {
-    pub fn new() -> Self {
-        FifoStatus
+    pub fn new(address: u8) -> Self {
+        FifoStatus { address }
     }
 
     /// Is the FIFO full
@@ -19,7 +21,7 @@ impl FifoStatus {
     where
         I2C: WriteRead,
     {
-        let v = self.read(i2c, ADDR + 1)?;
+        let v = self.read(i2c, self.address, ADDR + 1)?;
 
         Ok(v & (1 << 5) != 0)
     }
@@ -29,7 +31,7 @@ impl FifoStatus {
     where
         I2C: WriteRead,
     {
-        let v = self.read(i2c, ADDR + 1)?;
+        let v = self.read(i2c, self.address, ADDR + 1)?;
 
         Ok(v & (1 << 6) != 0)
     }
@@ -39,7 +41,7 @@ impl FifoStatus {
     where
         I2C: WriteRead,
     {
-        let v = self.read(i2c, ADDR + 1)?;
+        let v = self.read(i2c, self.address, ADDR + 1)?;
 
         Ok(v & (1 << 7) != 0)
     }
@@ -49,7 +51,7 @@ impl FifoStatus {
     where
         I2C: WriteRead,
     {
-        let v = self.read(i2c, ADDR + 1)?;
+        let v = self.read(i2c, self.address, ADDR + 1)?;
 
         Ok(v & (1 << 3) != 0)
     }
@@ -59,7 +61,7 @@ impl FifoStatus {
     where
         I2C: WriteRead,
     {
-        let v = self.read(i2c, ADDR + 1)?;
+        let v = self.read(i2c, self.address, ADDR + 1)?;
 
         Ok(v & (1 << 4) != 0)
     }
@@ -69,10 +71,11 @@ impl FifoStatus {
     where
         I2C: WriteRead,
     {
-        let v0 = self.read(i2c, ADDR + 0)?;
-        let v1 = self.read(i2c, ADDR + 1)? & 0b11;
+        let mut v = [0u8; 2];
+        i2c.write_read(self.address, &[ADDR], &mut v)?;
+        v[1] &= 0b11;
 
-        Ok(u16::from_le_bytes([v0, v1]))
+        Ok(u16::from_le_bytes(v))
     }
 }
 
@@ -83,22 +86,21 @@ mod tests {
 
     #[test]
     fn test_full() {
-        let mut i2c = Mock::new(&[
-            Transaction::write_read(0x6a, vec![0x3b], vec![0b100000]),
-        ]);
+        let mut i2c = Mock::new(&[Transaction::write_read(0x6b, vec![0x3b], vec![0b100000])]);
 
-        let mut f = FifoStatus;
+        let mut f = FifoStatus::new(crate::DEFAULT_I2C_ADDRESS);
         assert!(f.full(&mut i2c).unwrap());
     }
 
     #[test]
     fn test_diff_1b() {
-        let mut i2c = Mock::new(&[
-            Transaction::write_read(0x6a, vec![0x3a], vec![0b00100000_u8]),
-            Transaction::write_read(0x6a, vec![0x3b], vec![0b00000000_u8]),
-        ]);
+        let mut i2c = Mock::new(&[Transaction::write_read(
+            0x6b,
+            vec![0x3a],
+            vec![0b00100000_u8, 0b00000000_u8],
+        )]);
 
-        let mut f = FifoStatus;
+        let mut f = FifoStatus::new(crate::DEFAULT_I2C_ADDRESS);
         let diff = f.diff_fifo(&mut i2c).unwrap();
 
         assert_eq!(diff, 0b000100000);
@@ -106,12 +108,13 @@ mod tests {
 
     #[test]
     fn test_diff_2b() {
-        let mut i2c = Mock::new(&[
-            Transaction::write_read(0x6a, vec![0x3a], vec![0b00100000_u8]),
-            Transaction::write_read(0x6a, vec![0x3b], vec![0b00000001_u8]),
-        ]);
+        let mut i2c = Mock::new(&[Transaction::write_read(
+            0x6b,
+            vec![0x3a],
+            vec![0b00100000_u8, 0b00000001_u8],
+        )]);
 
-        let mut f = FifoStatus;
+        let mut f = FifoStatus::new(crate::DEFAULT_I2C_ADDRESS);
         let diff = f.diff_fifo(&mut i2c).unwrap();
 
         assert_eq!(diff, 0b100100000);
